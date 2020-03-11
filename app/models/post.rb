c0e1ed642ad9b3e14163bf7f6c37bd6448766ec9
andresporras3423 <andresporras3423@gmail.com>
2020-03-11 15:24:36 -0500

    created User and Post tables

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 500 }
+  validates :title, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
+  belongs_to :user
+end
