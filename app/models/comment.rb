4d15776ff521e6f5d90dc428e6c8eda1be4e9cbe
andresporras3423 <andresporras3423@gmail.com>
2020-03-12 10:11:48 -0500

    last changes and validations in stickler

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 550 }
-  validates :user_id, presence: true,
-  validates :post_id, presence: true,
+  validates :user_id, presence: true
+  validates :post_id, presence: true
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
   validates :title, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   belongs_to :user
-  has_many :comment
+  has_many :comments
 end
